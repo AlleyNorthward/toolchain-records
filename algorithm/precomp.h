4d398b1c98af01c87cbf1414f34e5f34bac02c12
AlleyNorthWard <wqianlong93@gmail.com>
2026-02-18 14:31:51 +0800

    算法

---
+++ algorithm/precomp.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <numbers>
+#include <vector>
+#include "timer.h"
+#include "graph.h"
+#include <list>
+#include <queue>
+
+
+
+
+
+
+
+
+
+
+
+
--- c++/study/practice.cpp
+++ c++/study/practice.cpp
@@ -1,23 +1,19 @@
-inline int add(int a, int b) noexcept{
-  return a + b;
-}
-virtual std::string name() const override
-int main(){
-  const int x = 10;
-  static double* p = nullptr;
-  int a[5]{1, 2, 4};
+struct A{
+  int a;
+};
 
-  int (*fp)(int, int) = add;
-  constexpr const int* (*ff[5])(double) noexcept;
+struct B:A{
+  int b;
+};
 
-  // 当然, 为了缓解, 现代C++提供了一系列缓解方案.
-  // using
-  using FP = int(*)(int, int);
-  //auto
-  auto fpp = add;
-  //decltype 没怎么用过, 但确实不错, 可以获取对象类型
-  decltype(add)* fppp;
-  fppp = add;
-  // trailing return Ai的, 没用过
-  auto f(int x) -> int;
-}
+
+
+
+
+
+
+
+
+
+
+
